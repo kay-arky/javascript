9fdfb1edd2f215043beabdde07808e0d18cec772
kay-arky <kay190505@gmail.com>
2025-02-11 07:24:23 +0800

    Math Methods

--- beginners/js/script.js
+++ beginners/js/script.js
@@ -1,45 +1,18 @@
-// Numbers
-// int number
-const myNumber = 20
-// A number with a decimal point is a float which references the "floating point"
-const myFloat = 20.01
-const myString = "20.123abc"
+// Math Methods and Properties
+console.log(Math.PI)
 
-console.log(myNumber)
-console.log(myFloat)
-console.log(myString)
-console.log(myFloat === myNumber)
-console.log(myString === myNumber)
-console.log(myString + 3)
-console.log(Number(myString) + 3)
-console.log(Number(myString) === myNumber)
-console.log(Number("Hizkia"))
-console.log(Number(true))
-
-// Number Methods
-// Number.isInteger() method determines whether the passed value is an integer
-console.log(Number.isInteger(myFloat))
-
-// Number.parseFloat() method parses an argument and returns a floating point number. If a number cannot be parsed from the argument, it returns NaN
-console.log(Number.parseFloat(myString))
-
-// toFixed() method formats a number according to how many decimal points you provide as the parameter
-console.log(myFloat.toFixed(2))
-
-// parseInt() method parses a string argument and returns an integer
-console.log(Number.parseInt(myString))
-
-//toString() method returns a string representing the number
-console.log(myFloat.toString())
-console.log(typeof Number.parseFloat(myString))
-
-// Chaining = Using several methods chained together
-console.log(Number.parseFloat("4.237abc").toFixed(2).toString())
-
-// NaN is an acronym for Not a Number
-// Number.isNaN() method determines whether the passed value is NaN AND its type is Number
-console.log(Number.isNaN("Hizkia"))
-console.log(Number("Hizkia"))
-
-// global isNaN() function determines whether a value is NaN or not
-console.log(isNaN("Hizkia"))
+console.log(Math.trunc(Math.PI))
+console.log(Math.round(Math.PI))
+console.log(Math.ceil(Math.PI))
+console.log(Math.floor(Math.PI))
+console.log("")
+console.log(Math.pow(2, 4))
+console.log(Math.min(2, 5, 9))
+console.log(Math.max(2, 5, 9))
+console.log(Math.random())
+// From 1 to 10
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
+console.log(Math.floor(Math.random() * 10) + 1)
