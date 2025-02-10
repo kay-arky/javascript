8dabb37b07525ceff793e48570018a94e036e629
kay-arky <kay190505@gmail.com>
2025-02-11 07:00:22 +0800

    Strings

--- beginners/js/script.js
+++ beginners/js/script.js
@@ -1,4 +1,15 @@
-console.log("Hello World")
-console.log(typeof "Dave")
-console.log(typeof 42)
-console.log(typeof true)
+// Strings
+const myVariable = "Mathematic"
+
+// The length property
+console.log(myVariable.length)
+
+// Strings Methods
+console.log(myVariable.charAt(0))
+console.log(myVariable.indexOf("ati"))
+console.log(myVariable.lastIndexOf("ath"))
+console.log(myVariable.slice(5, 6))
+console.log(myVariable.toUpperCase())
+console.log(myVariable.toLowerCase())
+console.log(myVariable.includes("mat"))
+console.log("Hizkia,Albert,Arundaa".split(","))
