7326e97d4a3dee41b56242c89dd840c29176eddb
kay-arky <kay190505@gmail.com>
2025-02-11 07:18:17 +0800

    numbers

--- beginners/js/script.js
+++ beginners/js/script.js
@@ -1,15 +1,45 @@
-// Strings
-const myVariable = "Mathematic"
+// Numbers
+// int number
+const myNumber = 20
+// A number with a decimal point is a float which references the "floating point"
+const myFloat = 20.01
+const myString = "20.123abc"
 
-// The length property
-console.log(myVariable.length)
+console.log(myNumber)
+console.log(myFloat)
+console.log(myString)
+console.log(myFloat === myNumber)
+console.log(myString === myNumber)
+console.log(myString + 3)
+console.log(Number(myString) + 3)
+console.log(Number(myString) === myNumber)
+console.log(Number("Hizkia"))
+console.log(Number(true))
 
-// Strings Methods
-console.log(myVariable.charAt(0))
-console.log(myVariable.indexOf("ati"))
-console.log(myVariable.lastIndexOf("ath"))
-console.log(myVariable.slice(5, 6))
-console.log(myVariable.toUpperCase())
-console.log(myVariable.toLowerCase())
-console.log(myVariable.includes("mat"))
-console.log("Hizkia,Albert,Arundaa".split(","))
+// Number Methods
+// Number.isInteger() method determines whether the passed value is an integer
+console.log(Number.isInteger(myFloat))
+
+// Number.parseFloat() method parses an argument and returns a floating point number. If a number cannot be parsed from the argument, it returns NaN
+console.log(Number.parseFloat(myString))
+
+// toFixed() method formats a number according to how many decimal points you provide as the parameter
+console.log(myFloat.toFixed(2))
+
+// parseInt() method parses a string argument and returns an integer
+console.log(Number.parseInt(myString))
+
+//toString() method returns a string representing the number
+console.log(myFloat.toString())
+console.log(typeof Number.parseFloat(myString))
+
+// Chaining = Using several methods chained together
+console.log(Number.parseFloat("4.237abc").toFixed(2).toString())
+
+// NaN is an acronym for Not a Number
+// Number.isNaN() method determines whether the passed value is NaN AND its type is Number
+console.log(Number.isNaN("Hizkia"))
+console.log(Number("Hizkia"))
+
+// global isNaN() function determines whether a value is NaN or not
+console.log(isNaN("Hizkia"))
