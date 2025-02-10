1693642bb7a2a1a4194da30f99d4cbd0548fa19b
kay-arky <kay190505@gmail.com>
2025-02-11 07:31:40 +0800

    first code callenge

--- beginners/js/script.js
+++ beginners/js/script.js
@@ -1,18 +1,4 @@
-// Math Methods and Properties
-console.log(Math.PI)
-
-console.log(Math.trunc(Math.PI))
-console.log(Math.round(Math.PI))
-console.log(Math.ceil(Math.PI))
-console.log(Math.floor(Math.PI))
-console.log("")
-console.log(Math.pow(2, 4))
-console.log(Math.min(2, 5, 9))
-console.log(Math.max(2, 5, 9))
-console.log(Math.random())
-// From 1 to 10
-console.log(Math.floor(Math.random() * 10) + 1)
-console.log(Math.floor(Math.random() * 10) + 1)
-console.log(Math.floor(Math.random() * 10) + 1)
-console.log(Math.floor(Math.random() * 10) + 1)
-console.log(Math.floor(Math.random() * 10) + 1)
+// First Code Challenge
+const myName = "kay-arky"
+const randIndex = Math.floor(Math.random() * myName.length)
+console.log("Random letter: " + myName.charAt(randIndex))
